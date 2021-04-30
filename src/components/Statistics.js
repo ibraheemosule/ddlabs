33e423a760e3b2ae127c22f271f8cc4c01d8510a
sulayibraheem <sulayibraheem@gmail.com>
2021-04-30 22:36:11 +0100

    completed the list component

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,15 +1,28 @@
-import {Row, Col} from "react-bootstrap"
+import {Row, Col, Button } from "react-bootstrap"
 import '../assets/Statistics.scss';
+
 const Statistics = () => {
   return (
     <Row className="statistics-container">
       <Col xs={3} className="items">
       <Row className="header">
-        <h4>Categories</h4>
+        <h4>Profitable items</h4>
         <h4>. .</h4>
+      </Row>
+      <Row className="lists">
+        <ol>
+          <li>Bakery <span>+ $12</span></li>
+          <li>Electronics <span className="negative">- $12</span></li>
+          <li>Plants <span>+ $12</span></li>
+        </ol>
+        <Button>See all activities</Button>
       </Row>
       </Col>
       <Col xs={8}className="sales">
+      <Row className="header">
+        <h4>Sales statistics</h4>
+        <h4>. .</h4>
+      </Row>
       </Col>
     </Row>
   )
